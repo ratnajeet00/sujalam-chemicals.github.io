956f110b587969481b4bdbeda05f6592601d1c58
Arya Maddel <maddelarya@gmail.com>
2023-06-28 17:20:03 +0530

    Create [username].js

---
+++ app/[username].js
@@ -0,0 +1,17 @@
+import { Stack, useGlobalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
+
+
+export default function Profile() {
+    const {name, username} = useGlobalSearchParams()
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Stack.Screen
+            options={{
+            headerTitle: `Welcome ${name}`,
+            }}
+        />
+        <Text>Username: {name} (@{username})</Text>
+        </View>
+    );
+}
