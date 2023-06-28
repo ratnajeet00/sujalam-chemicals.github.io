7c00252cd1d1d656ecb5d17a8ac404fff3e8b819
Arya Maddel <maddelarya@gmail.com>
2023-06-28 17:16:45 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,11 +1,10 @@
+import { Link } from "expo-router";
 import { View } from "react-native";
-import { Stack, useRouter } from "expo-router";
 
 export default function Home() {
-  const router = useRouter();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Stack.Screen options={{ title: "Dashboard" }} />
+      <Link href="(auth)/login">Login</Link>
     </View>
   );
 }
