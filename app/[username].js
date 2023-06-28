c0200231ccf24215e6664df0a729a157147e5748
Arya Maddel <maddelarya@gmail.com>
2023-06-28 23:20:47 +0530

    Update [username].js

--- app/[username].js
+++ app/[username].js
@@ -2,17 +2,18 @@ import { Stack, useGlobalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Profile() {
-  const { name, username } = useGlobalSearchParams();
+  const { name, lastName, email, username } = useGlobalSearchParams();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Stack.Screen
         options={{
-          headerTitle: `Welcome ${name}`,
+          headerTitle: name + " " + lastName,
         }}
       />
       <Text>
         Username: {name} (@{username})
       </Text>
+      <Text>email: {email}</Text>
     </View>
   );
 }
