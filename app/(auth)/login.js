51d5fd759255953a6e7ea28958e738ce61acc54b
Arya Maddel <maddelarya@gmail.com>
2023-07-06 02:14:49 +0530

    Moree LOGINNN

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -1,22 +1,28 @@
-import { Text, View } from "react-native";
-import { AuthStore } from "../../store.js";
 import { Stack, useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import SimpleInput from "../../components/Inputs/SimpleInput.js";
+import { AuthStore } from "../../store.js";
 
 export default function LogIn() {
   const router = useRouter();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Stack.Screen options={{ title: "Login" }} />
-      <Text
-        onPress={() => {
-          AuthStore.update((s) => {
-            s.isLoggedIn = true;
-          });
-          router.replace("/(pages)/home");
-        }}
-      >
-        Login
-      </Text>
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Stack.Screen options={{ title: "Login" }} />
+        <SimpleInput />
+        <Text
+          onPress={() => {
+            AuthStore.update((s) => {
+              s.isLoggedIn = true;
+            });
+            router.replace("/(pages)/home");
+          }}
+        >
+          Login
+        </Text>
+      </View>
+    </>
   );
 }
---
+++ components/Inputs/SimpleInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SimpleInput = () => {
+  return (
+    <View>
+      <Text>SimpleInput</Text>
+    </View>
+  );
+};
+
+export default SimpleInput;
