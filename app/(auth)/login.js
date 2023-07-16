749a168e5499f590793cf6c6767e840a03113b30
Arya Maddel <maddelarya@gmail.com>
2023-07-16 22:50:20 +0530

    faster login

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -20,14 +20,14 @@ export default function LogIn() {
   const { height } = useWindowDimensions();
   const [username, setEmployeeId] = useState("");
   const [password, setPassword] = useState("");
-  const onSignInPressed = async () => {
-    setIsLoading(true);
+
+  const handleSignIn = async () => {
     if (!username || !password) {
       alert("Please enter both username and password");
-      setIsLoading(false);
       return;
     }
 
+    setIsLoading(true);
     try {
       const body = JSON.stringify({
         username: username,
@@ -35,7 +35,7 @@ export default function LogIn() {
       });
 
       const response = await fetch(
-        "https://eminent-quickest-menu.glitch.me/login",
+        "https://dbd4-2405-201-4014-21e-74ac-180d-a3b4-ef2b.ngrok-free.app/login",
         {
           method: "POST",
           headers: {
@@ -52,14 +52,16 @@ export default function LogIn() {
         });
         router.replace("/(pages)/home");
       } else {
-        alert("Login failed");
+        alert("Login failed. Please check your credentials.");
       }
     } catch (error) {
       console.error(error);
+      alert("An error occurred during login.");
     } finally {
       setIsLoading(false);
     }
   };
+
   const onForgotPasswordPressed = () => {
     router.push("/forgotPassword");
   };
@@ -72,24 +74,32 @@ export default function LogIn() {
         resizeMode="contain"
         style={[styles.logo, { height: height * 0.3 }]}
       />
-      <CustomInput
-        placeholder="Employee ID"
-        value={username}
-        setValue={setEmployeeId}
-      />
-      <CustomInput
-        placeholder="Password"
-        value={password}
-        setValue={setPassword}
-        secureTextEntry={true}
-      />
-      <CustomButton onPress={onSignInPressed} text="Sign In" type="PRIMARY" />
-      {isLoading && <ActivityIndicator size="large" />}
-      <CustomButton
-        onPress={onForgotPasswordPressed}
-        text="Forgot password?"
-        type="TERTIARY"
-      />
+      <View style={styles.formContainer}>
+        <CustomInput
+          placeholder="Employee ID"
+          value={username}
+          setValue={setEmployeeId}
+        />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+        />
+        <CustomButton
+          onPress={handleSignIn}
+          text="Sign In"
+          type="PRIMARY"
+          disabled={isLoading}
+        />
+        {isLoading && <ActivityIndicator size="large" />}
+        <CustomButton
+          onPress={onForgotPasswordPressed}
+          text="Forgot password?"
+          type="TERTIARY"
+          disabled={isLoading}
+        />
+      </View>
     </View>
   );
 }
@@ -105,5 +115,9 @@ const styles = StyleSheet.create({
   logo: {
     width: "70%",
     maxWidth: 300,
+  },
+  formContainer: {
+    width: "100%",
+    marginTop: 20,
   },
 });
